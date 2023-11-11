66ea8cb8387ae5c465084f07bdb796f2a33106f6
jankooovic <92938659+jankooovic@users.noreply.github.com>
2023-11-11 10:38:01 +0100

    ...

--- yolov8_predict.py
+++ yolov8_predict.py
@@ -10,7 +10,7 @@ save_path = "./data/predicted"
 test_img_path = "/images/test/"
 point_names = ['FHC', 'TKC', 'TML', 'FNOC', 'aF1', 'ALL']
 landmark_names = ['FHC', 'TKC', 'TML', 'FNOC', 'aF1']
-model_paths = { "ALL" : "./runs/pose/trained_ALL_grayscale/weights/best.pt" }
+model_paths = { "ALL" : "./runs/pose/train_ALL_960_grayscale/weights/best.pt" }
 
 # create dataset archive
 yolov8_functions.dataset_archive(save_path)
