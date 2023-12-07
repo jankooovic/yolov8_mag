c5731cb94c77ae53d933d8d2e1e11653dd4bde46
jankooovic <92938659+jankooovic@users.noreply.github.com>
2023-12-07 12:40:10 +0100

    ...

--- yolov8_train.py
+++ yolov8_train.py
@@ -26,38 +26,38 @@ lr0 = 0.01 - 0.0001
 Sample command: model.train(data='config.yaml', epochs=100, imgsz=640) 
 """
 
-learning_rates = [0.1, 0.001, 0.0001]
+learning_rates = [0.01] #0.1, 0.001, 0.0001
 optimizers = ["Adam", "SGD", "Adamax"]
-img_sizes = [960, 1280, 1920, 2016, 3040, 3680]
-imgsz = 960
+img_sizes = [960, 1920] #960, 1280, 1920, 2016, 3040, 3680
 config_files = ['config/config_ALL.yaml']
 
 ### Train model - per config file
 for config in config_files:
-    for optimizer in optimizers:
-        for lr in learning_rates:
+    for imgsize in img_sizes:
+        for optimizer in optimizers:
+            for lr in learning_rates:
 
-            print("Using config file:", config)
-            print("using optimizer:", optimizer)
-            print("Using learning rate:", lr)
+                print("Using config file:", config)
+                print("using optimizer:", optimizer)
+                print("Using learning rate:", lr)
 
-            model.train(
-                data=config,
-                pretrained=True,
-                epochs=300,
-                batch=16,
-                # Test parameters
-                imgsz=imgsz,
-                lr0 = lr,
-                optimizer=optimizer,
-                # Data augemntation parameters
-                hsv_h = 0.015,
-                hsv_s = 0.1,
-                hsv_v = 0.05,
-                degrees=10,
-                scale=0.1,
-                perspective=0.001,
-                translate=0,
-                fliplr=0,
-                mosaic=0
-                )
+                model.train(
+                    data=config,
+                    pretrained=True,
+                    epochs=300,
+                    batch=16,
+                    # Test parameters
+                    imgsz=imgsize,
+                    lr0 = lr,
+                    optimizer=optimizer,
+                    # Data augemntation parameters
+                    hsv_h = 0.015,
+                    hsv_s = 0.1,
+                    hsv_v = 0.05,
+                    degrees=10,
+                    scale=0.1,
+                    perspective=0.001,
+                    translate=0,
+                    fliplr=0,
+                    mosaic=0
+                    )
