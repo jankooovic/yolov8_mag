b8c99e6a0fbd16aaea401f172e52e7efd34000d2
jankooovic <92938659+jankooovic@users.noreply.github.com>
2023-12-10 14:48:17 +0100

    ....

--- yolov8_train.py
+++ yolov8_train.py
@@ -11,53 +11,46 @@ model = YOLO('yolov8n-pose.yaml').load('yolov8n-pose.pt')   # build from YAML an
 hsv_h = 0.015 image HSV-Hue augmentation (fraction)
 hsv_s = 0.7	image HSV-Saturation augmentation (fraction)
 hsv_v = 0.4	image HSV-Value augmentation (fraction)
+
 degrees = 0.0 image rotation (+/- deg)
 scale = 0.5 image scale (+/- gain)
-shear = 0.0	image shear (+/- deg)
 perspective = 0.0 image perspective (+/- fraction), range 0-0.001
-flipud = 0.0 image flip up-down (probability)
 fliplr = 0.5 image flip left-right (probability)
 imgsz = 640	image size as scalar or (h, w) list, i.e. (640, 480)
 batch = 16 number of images per batch (-1 for AutoBatch)
-
-optimizer = SGD, Adam, Adamax
-lr0 = 0.01 - 0.0001
+optimizer = Adam
+lr0 = 0.01
 
 Sample command: model.train(data='config.yaml', epochs=100, imgsz=640) 
 """
 
-learning_rates = [0.01] #0.1, 0.001, 0.0001
-optimizers = ["Adam", "SGD", "Adamax"]
-img_sizes = [960, 1920] #960, 1280, 1920, 2016, 3040, 3680
-config_files = ['config/config_ALL.yaml']
+img_sizes = [1920] #960, 1280, 1920, 2016, 3040, 3680
+degrees = [0, 10]
+scales = [0, 0.1]
+perspectives = [0, 0.001]
+config = 'config/config_ALL.yaml'
 
 ### Train model - per config file
-for config in config_files:
-    for imgsize in img_sizes:
-        for optimizer in optimizers:
-            for lr in learning_rates:
+for imgsize in img_sizes:
+    for idx, perspective in enumerate(perspectives):
 
-                print("Using config file:", config)
-                print("using optimizer:", optimizer)
-                print("Using learning rate:", lr)
-
-                model.train(
-                    data=config,
-                    pretrained=True,
-                    epochs=300,
-                    batch=16,
-                    # Test parameters
-                    imgsz=imgsize,
-                    lr0 = lr,
-                    optimizer=optimizer,
-                    # Data augemntation parameters
-                    hsv_h = 0.015,
-                    hsv_s = 0.1,
-                    hsv_v = 0.05,
-                    degrees=10,
-                    scale=0.1,
-                    perspective=0.001,
-                    translate=0,
-                    fliplr=0,
-                    mosaic=0
-                    )
+        model.train(
+            data=config,
+            imgsz=imgsize,
+            pretrained=True,
+            epochs=300,
+            batch=16,
+            lr0 = 0.01,
+            optimizer="Adam",
+            # Data augemntation parameters
+            degrees=degrees[idx],
+            scale=scales[idx],
+            perspective=perspectives[idx],
+            # annoyance
+            translate=0,
+            fliplr=0,
+            mosaic=0,
+            hsv_h = 0,
+            hsv_s = 0,
+            hsv_v = 0,
+        )
